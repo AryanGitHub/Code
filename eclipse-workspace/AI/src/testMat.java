4d4755fb1ff064fabe2638158d26c6d11c78677b
AryanGitHub <aryanandroid26@gmail.com>
2018-03-27 03:27:55 +0530

    Adding eclipse-workspace

---
+++ eclipse-workspace/AI/src/testMat.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class testMat {
+
+	public static void main(String[] args) {
+
+		
+		     
+		      Matrix m = Matrix.rand(10,10, 9);
+		      
+		      
+		      
+		   
+		      
+		      m.display();
+		     
+		    }
+
+
+		}
+
+
